79a3ac45021194bbcaf14ee213c0e9292bd6f5bd
Alexandria Roberts <iamlexieonearth@gmail.com>
2021-04-08 15:19:20 +0800

    Added password react validation

--- src/components/SetPassword.js
+++ src/components/SetPassword.js
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 
 export default function SetPassword({ setStep, formValues, setFormValues }) {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, errors } = useForm();
 
   function onSubmit(values) {
     setFormValues({
@@ -15,15 +15,45 @@ export default function SetPassword({ setStep, formValues, setFormValues }) {
       <form onSubmit={handleSubmit(onSubmit)} className="px-10">
         <div className="flex flex-col mb-4">
           <label
-            htmlFor="name"
+            htmlFor="password"
             className="mb-2 uppercase font-bold text-lg text-grey-darkest"
           ></label>
 
+          {errors.password && <p>{errors.password.message}</p>}
+
           <input
-            ref={register}
-            name="name"
-            type="text"
-            placeholder="Name"
+            ref={register({
+              required: "You must specify a password",
+              minLength: {
+                value: 8,
+                message: "Password must have at least 8 characters",
+              },
+            })}
+            name="password"
+            type="password"
+            placeholder="Password"
+            className="border py-2 px-3 text-grey-darkest rounded-lg shadow-sm"
+          />
+        </div>
+        <div className="flex flex-col mb-4">
+          <label
+            htmlFor="repeat password"
+            className="mb-2 uppercase font-bold text-lg text-grey-darkest"
+          ></label>
+
+          {errors.password && <p>{errors.password.message}</p>}
+
+          <input
+            ref={register({
+              required: "You must repeat your password",
+              minLength: {
+                value: 8,
+                message: "Password must have at least 8 characters",
+              },
+            })}
+            name="password"
+            type="password"
+            placeholder="Repeat Password"
             className="border py-2 px-3 text-grey-darkest rounded-lg shadow-sm"
           />
         </div>
